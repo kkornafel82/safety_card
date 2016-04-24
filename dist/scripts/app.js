34dc9df12bada5141a930dfec0d2c1ea450769fc
Kevin <kkornafel82@yahoo.com>
2016-04-24 13:56:41 -0700

    progress on navbar

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -8,8 +8,28 @@
 
         $stateProvider
           .state('landing', {
-             url: '/landing',
-             templateUrl: '/templates/landing.html'
+            url: '/landing',
+            templateUrl: '/templates/landing.html'
+          })
+
+        .state('whycsc', {
+            url: '/whycsc',
+            templateUrl: '/templates/whycsc.html'
+          })
+
+        .state('samples', {
+            url: '/samples',
+            templateUrl: '/templates/samples.html'
+          })
+        
+        .state('order', {
+            url: '/order',
+            templateUrl: '/templates/order.html'
+          })
+
+        .state('contactus', {
+            url: '/contactus',
+            templateUrl: '/templates/contactus.html'
           });
  
         $urlRouterProvider.otherwise("/landing");
